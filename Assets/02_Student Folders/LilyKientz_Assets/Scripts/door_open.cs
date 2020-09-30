40f6743834b71ef391c0273bcb52c70e339fc311
izergis <38942342+izergis@users.noreply.github.com>
2020-09-30 17:08:20 +0200

    starting to work on the door

---
+++ Assets/02_Student Folders/LilyKientz_Assets/Scripts/door_open.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class hiddenroom : MonoBehaviour
+{
+    public float speed = 1f;
+    bool isOpen = false;
+    bool isClosing = false;
+    float timer;
+    float timerLength = 1f;
+
+    public Transform top;
+    public Transform bottom;
+    public Transform topleft;
+    public Transform topright;
+    public Transform bottomleft;
+    public Transform bottomright;
+    public Collider hitbox;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen && timer > 0f)
+        {
+            hatch1.Translate(Vector3.left * Time.deltaTime * speed);
+            hatch2.Translate(Vector3.right * Time.deltaTime * speed);
+            lift.Translate(Vector3.up * Time.deltaTime * speed * 2);
+            robot.Translate(Vector3.up * Time.deltaTime * speed * 2);
+            timer -= Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            timer = timerLength;
+        }
+    }
+}
