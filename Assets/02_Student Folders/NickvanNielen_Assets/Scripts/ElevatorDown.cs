028affb1b8bb5b6e5b670f24468f3403fc9643c8
Codemaster44 <Vexen9games@gmail.com>
2020-10-07 13:49:19 +0200

    Added Elevator+ sound, level in progress

---
+++ Assets/02_Student Folders/NickvanNielen_Assets/Scripts/ElevatorDown.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDown : MonoBehaviour
+{
+    public Transform elevator;
+
+    private bool goDown = false;
+    private bool playOnce = true;
+
+    private float timer = 20f;
+
+    public float speed = 2f;
+
+    public AudioSource sound1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goDown && timer > 0f) {
+            elevator.Translate(Vector3.down * Time.deltaTime * speed);
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0f && playOnce) {
+            playOnce = false;
+            sound1.Play();
+        }
+    }
+
+    public void moveDown() {
+        goDown = !goDown;
+    }
+}
---
+++ Assets/02_Student Folders/NickvanNielen_Assets/Scripts/InteractButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractButton : MonoBehaviour
+{
+    public bool inRange;
+    public KeyCode pressable;
+    public UnityEvent interactAction;
+    public GameObject Player;
+    public AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange) {
+            if (Input.GetKeyDown(pressable)) {
+                interactAction.Invoke();
+                sound.Play();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        if(collision.gameObject == Player) {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision) {
+        if(collision.gameObject == Player) {
+            inRange = false;
+        }
+    }
+}
