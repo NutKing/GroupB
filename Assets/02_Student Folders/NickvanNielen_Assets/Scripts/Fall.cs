a710ae89f41f7a32b66a504d3bc11dc39e50842d
Codemaster44 <Vexen9games@gmail.com>
2020-10-09 00:09:26 +0200

    Block exit

---
+++ Assets/02_Student Folders/NickvanNielen_Assets/Scripts/Fall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    private bool fall = false;
+
+    public AudioSource sound1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fall) {
+            transform.Rotate((float)24, (float)40, 0);
+            transform.position -= new Vector3(0, (float)3.5, 0);
+            sound1.Play();
+            fall = false;
+        }
+    }
+
+    public void setFall() {
+        fall = true;
+    }
+}
