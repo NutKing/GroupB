03e72ca6a213b4a54794706797d84863c3847c7d
TypeA2 <nuanlim29@gmail.com>
2020-10-11 23:28:25 +0200

    Moving doors

--- Assets/02_Student Folders/NuanLim_Assets/Scripts/Trigger.cs
+++ Assets/02_Student Folders/NuanLim_Assets/Scripts/Trigger.cs
@@ -2,51 +2,44 @@
 using System.Linq;
 using Assets._02_Student_Folders.NuanLim_Assets.Scripts;
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(Health))]
 public class Trigger : MonoBehaviour {
 
     private Health _health;
 
-    private bool _dead;
-    private List<(Transform, Vector3, bool)> _keys;
-    
+    private bool _moving;
+    private Vector3 _moved;
+
+    public UnityAction OnTrigger;
+
     public List<GameObject> keys;
-    public float distance = -1f;
+    public float distance = 1f;
     public float velocity = 1f;
+    public Vector3 movement = Vector3.down;
 
     // Start is called before the first frame update
-    private void Start() {
+    protected void Start() {
         _health = GetComponent<Health>();
-        _health.onDie += OnDie;
-
-        _keys = keys.Select(k => (k.transform, k.transform.position.copy(), false)).ToList();
-
+        _health.onDie += () => {
+            _moving = true;
+            OnTrigger.Invoke();
+        };
     }
 
     // Update is called once per frame
-    private void Update() {
-        if (_dead && _keys.Any(e => e.Item3)) {
-            Vector3 movement = Vector3.down * velocity * Time.deltaTime;
+    protected void Update() {
+        if (_moving) {
+            Vector3 move = movement * velocity * Time.deltaTime;
 
-            for (int i = 0; i < _keys.Count; ++i) {
-                (Transform t, Vector3 start, bool lowering) = _keys[i];
-                if (!lowering) {
-                    continue;
-                }
+            keys.ForEach(o => o.transform.Translate(move));
 
-                t.Translate(movement);
-
-                if (t.position.y - start.y < distance) {
-                    _keys[i] = (t, start, false);
-                }
+            _moved += move;
+            if (_moved.magnitude > (movement * distance).magnitude) {
+                _moving = false;
+                // Destroying the objects gave a bunch of errors so whatever
             }
-        }
-    }
-
-    private void OnDie() {
-        _dead = true;
-        for (int i = 0; i < _keys.Count; ++i) {
-            _keys[i] = (_keys[i].Item1, _keys[i].Item2, true);
         }
     }
 }
---
+++ Assets/02_Student Folders/NuanLim_Assets/Scripts/TriggerDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Trigger))]
+public class TriggerDoor : MonoBehaviour {
+
+    private Trigger _trigger;
+
+    private bool _moving;
+    private Vector3 _moved;
+
+    public List<GameObject> objects;
+    public float distance = 9f;
+    public float velocity = 1f;
+    public Vector3 movement = Vector3.down;
+
+    // Use this for initialization
+    protected void Start() {
+        _trigger = GetComponent<Trigger>();
+        _trigger.OnTrigger += () => _moving = true;
+    }
+
+    // Update is called once per frame
+    protected void Update() {
+        if (_moving) {
+            Vector3 move = movement * velocity * Time.deltaTime;
+
+            objects.ForEach(o => o.transform.Translate(move));
+
+            _moved += move;
+            if (_moved.magnitude > (movement * distance).magnitude) {
+                _moving = false;
+            }
+        }
+    }
+
+}
