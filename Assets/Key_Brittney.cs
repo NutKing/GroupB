fba063735e868dfadc4e9e23ad841890f4017cbc
Brittney <b.jansen2104@gmail.com>
2020-10-14 16:09:53 +0200

    Create Key_Brittney.cs

---
+++ Assets/Key_Brittney.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_Brittney : MonoBehaviour
+{
+
+    [SerializeField] private KeyType Keytype;
+
+    public enum KeyType
+    {
+        Purple,
+        Blue
+    }
+
+    public KeyType GetKeyType()
+    {
+        return KeyType; 
+    }
+}
