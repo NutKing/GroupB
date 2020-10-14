3d84faaa78835e538ad3ee2de563a3ead2fc2aad
Brittney <b.jansen2104@gmail.com>
2020-10-14 16:09:37 +0200

    Create Keyholder.cs

---
+++ Assets/02_Student Folders/BrittneyJansen_Assets/Scripts/Keyholder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyholder : MonoBehaviour
+{
+
+    private List<Key.KeyType> keyList;
+
+    private void Awake()
+    {
+        keylist = new List<Key.KeyType>();
+    }
+
+    public void AddKey(Key.KeyType keyType)
+    {
+        Debug.Log("Added Key: " + keyType);
+        keyList.Add(Key.KeyType keyType)
+    }
+
+    public void RemoveKey(Key.KeyType keyType)
+    {
+        keyList.Remove(keyType)
+    }
+
+    public bool ContainsKey(Key.Keytype keytype)
+    {
+        return keyList.Contains(keytype);
+    }
+
+    private void OntriggerEnter2D(Collider2D collider)
+    {
+        Key key = collider.GetComponent<key>();
+        if (key != null)
+        {
+            AddKey(key.GetKeyType());
+            Destroy(key.gameObject);
+        }
+    }
+}
