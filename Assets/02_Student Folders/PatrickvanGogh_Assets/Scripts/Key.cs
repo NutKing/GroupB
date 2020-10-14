b98411371779828a480eaecc50ce966b72b62b04
patrickvangogh <patrickvangogh@gmail.com>
2020-10-14 14:05:14 +0200

    Started key scripts

---
+++ Assets/02_Student Folders/PatrickvanGogh_Assets/Scripts/Key.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] public KeyType keyType;
+
+    public enum KeyType
+    {
+        Iron,
+        Gold
+    }
+    public KeyType GetKeyType()
+    {
+        return keyType;
+    }
+}
---
+++ Assets/02_Student Folders/PatrickvanGogh_Assets/Scripts/KeyHolder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyHolder : MonoBehaviour {
+        private List<Key.KeyType> keyList;
+
+        private void Awake() {
+        keyList = new List<Key.KeyType>();
+    }
+
+    public void AddKey(Key.KeyType keyType) {
+        keyList.Add(keyType);
+        Debug.Log("Key picked up" + keyType); 
+    }
+
+    public void RemoveKey(Key.KeyType keyType) {
+        keyList.Remove(keyType);
+        Debug.Log("Key used" + keyType);
+    }
+    
+    public bool ContainsKey (Key.KeyType keyType) {
+        return keyList.Contains(keyType);
+    }
+   
+    private void OnTriggerEnter(Collider collider) {
+        Key key = collider.GetComponent<Key>();
+        if (key != null) {
+            AddKey(key.GetKeyType());
+            Destroy(key.gameObject);
+        }
+    }
+}
