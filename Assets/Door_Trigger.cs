f9fc0b459c9749318eda54f16de58e30ad7e9221
Brittney <b.jansen2104@gmail.com>
2020-10-19 01:23:08 +0200

    Create Door_Trigger.cs

---
+++ Assets/Door_Trigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Trigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Deur;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Deur.transform.position += new Vector3(0, -5.3f, 0);
+    }
+}
