7446e07116ca45645a1c5559d070da41319174b0
Brittney <b.jansen2104@gmail.com>
2020-10-19 23:53:43 +0200

    Update Door_Trigger.cs

--- Assets/Door_Trigger.cs
+++ Assets/Door_Trigger.cs
@@ -6,9 +6,13 @@ public class Door_Trigger : MonoBehaviour
 {
     [SerializeField]
     GameObject Deur;
+    public AudioSource Sound;
+    public ParticleSystem Partpart;
 
     public void OnTriggerEnter(Collider other)
     {
         Deur.transform.position += new Vector3(0, -5.3f, 0);
+        Sound.Play();
+        Partpart.Play();
     }
 }
