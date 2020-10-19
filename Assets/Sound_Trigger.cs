fe73e297b6b0cab2f37c83c9cc1c1e050f697904
Brittney <b.jansen2104@gmail.com>
2020-10-19 19:55:01 +0200

    Create Sound_Trigger.cs

---
+++ Assets/Sound_Trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Trigger : MonoBehaviour
+{
+    [SerializeField] AudioSource TriggerSound;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 0.5f)
+        {
+            TriggerSound.Play();
+        }
+    }
+}
