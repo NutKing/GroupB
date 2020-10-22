fdac10e2232f2265aa12d1c5bba2f7d01d51a1a9
TypeA2 <nuanlim29@gmail.com>
2020-10-22 17:43:10 +0200

    fix WebGL build

--- Assets/FPS/Scripts/UI/WorldspaceHealthBar.cs
+++ Assets/FPS/Scripts/UI/WorldspaceHealthBar.cs
@@ -14,9 +14,11 @@ public class WorldspaceHealthBar : MonoBehaviour
 
     void Update()
     {
-        // update health bar value
-        healthBarImage.fillAmount = health.currentHealth / health.maxHealth;
-        
+        if (healthBarImage) {
+            // update health bar value
+            healthBarImage.fillAmount = health.currentHealth / health.maxHealth;
+        }
+
         // rotate health bar to face the camera/player
         healthBarPivot.LookAt(Camera.main.transform.position);
 
